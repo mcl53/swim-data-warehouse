947629478b03a7620ce0270a611ed965c0018fbd
mcl53 <52954047+mcl53@users.noreply.github.com>
2023-12-26 22:36:41 +0000

    Update packages and fix page_event_number
    - Update the version number of required packages
    - Add additional missing rows in page_event_number by adding conditions for specific files in WHERE clause
    - Commenting to show which files each value in the WHERE correspond to

--- swim_data_warehouse_dbt/models/isl_dw/page_event_number.sql
+++ swim_data_warehouse_dbt/models/isl_dw/page_event_number.sql
@@ -5,8 +5,8 @@ SELECT
     loaded_datetime
 FROM
     {{ source('isl_raw', 'pdf_page_word') }}
-WHERE
-    ROUND(location_y, 5) IN (605.75611, 643.8682)
+WHERE                     -- Dallas , Other US, Vegas  , Other EU, London
+    ROUND(location_y, 3) IN (598.236, 605.756 , 638.408, 643.868 , 675.393)
 QUALIFY
     -- All files have 3 words that follow the pattern: ['Event', 'Number', 'x'], where x is the integer event number
     ROW_NUMBER() OVER (PARTITION BY file_name, page_number, loaded_datetime ORDER BY location_x ASC) = 3

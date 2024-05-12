454852fa76ccbf42c3d4c72689beae2acd962cc2
mcl53 <52954047+mcl53@users.noreply.github.com>
2024-05-12 17:43:09 +0100

    Update page_event_number to use new source

--- swim_data_warehouse_dbt/models/isl_dw/page/page_event_number.sql
+++ swim_data_warehouse_dbt/models/isl_dw/page/page_event_number.sql
@@ -1,18 +1,33 @@
-{{
-      config(
-        enabled = false
-        )
-}}
+WITH page_event_number_line_number AS (
+    SELECT
+        file_name,
+        page_number,
+        line_number
+    FROM
+        {{ ref('pdf_page_line_word') }}
+    WHERE
+        word = 'Event'
+    -- The word 'Event' appears twice in all files, as both have a system event number and a sequential event number.
+    -- Here we filter to only the second of these, the sequential event number.
+    QUALIFY
+        line_number = MAX(line_number) OVER (
+                          PARTITION BY
+                              file_name,
+                              page_number
+                      )
+)
 
 SELECT
-    file_name,
-    page_number,
-    word AS event_number,
-    loaded_datetime
+    word.file_name         AS file_name,
+    word.page_number       AS page_number,
+    CAST(word.word AS INT) AS event_number
 FROM
-    {{ source('isl_raw', 'pdf_page_word') }}
-WHERE                     -- Dallas , Other US, Vegas  , Other EU, London
-    ROUND(location_y, 3) IN (598.236, 605.756 , 638.408, 643.868 , 675.393)
-QUALIFY
-    -- All files have 3 words that follow the pattern: ['Event', 'Number', 'x'], where x is the integer event number
-    ROW_NUMBER() OVER (PARTITION BY file_name, page_number, loaded_datetime ORDER BY location_x ASC) = 3
+    {{ ref('pdf_page_line_word') }} word
+INNER JOIN
+    page_event_number_line_number   event_line
+ON
+    word.file_name   = event_line.file_name
+AND word.page_number = event_line.page_number
+AND word.line_number = event_line.line_number
+WHERE
+    word NOT IN ('Event', 'Number')
